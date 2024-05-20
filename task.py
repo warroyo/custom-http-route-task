84d26b34a5b1f07206aff1f77b8a6e4042876799
Will Arroyo <warroyo7199008@gmail.com>
2024-05-20 13:37:26 -0600

    initial

---
+++ task.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import yaml
+import os
+
+
+workspaceDir = os.environ.get('TANZU_BUILD_WORKSPACE_DIR')
+
+print("creating httproute")
+with open(workspaceDir + '/output/containerapp.yml', 'r') as file:
+    containerapp = yaml.safe_load(file)
+
+with open(workspaceDir + '/httproute.yml', 'r') as file:
+    httproute = yaml.safe_load(file)
+
+
+httproute["metadata"]["name"] = containerapp["metadata"]["name"] + "-http-route"
+httproute["metadata"]["annotations"]["healthcheck.gslb.tanzu.vmware.com/service"] = containerapp["metadata"]["name"]
+httproute["metadata"]["annotations"]["healthcheck.gslb.tanzu.vmware.com/port"] = "443"
+
+httproute["spec"]["parentRefs"][0]["sectionName"] = "https-"+containerapp["metadata"]["name"]
+httproute["spec"]["rules"][0]["backendRefs"][0]["name"] = containerapp["metadata"]["name"]
+httproute["spec"]["rules"][0]["backendRefs"][0]["port"] = containerapp["spec"]["ports"][0]["port"]
+
+with open(workspaceDir + '/output/httproute.yml', 'w') as outfile:
+    yaml.dump(httproute, outfile, default_flow_style=False)
